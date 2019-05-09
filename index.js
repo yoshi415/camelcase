dde5e976fd0e7bf74de87c816553324ffef47878
Yoshio Varney <yoshio.varney@gmail.com>
2019-05-09 14:21:05 -0700

    Transpile to es5

--- index.js
+++ index.js
@@ -1,71 +1,102 @@
-'use strict';
+"use strict";
 
-const preserveCamelCase = input => {
-	let isLastCharLower = false;
-	let isLastCharUpper = false;
-	let isLastLastCharUpper = false;
+var preserveCamelCase = function preserveCamelCase(string) {
+	var isLastCharLower = false;
+	var isLastCharUpper = false;
+	var isLastLastCharUpper = false;
 
-	for (let i = 0; i < input.length; i++) {
-		const c = input[i];
+	for (var i = 0; i < string.length; i++) {
+		var character = string[i];
 
-		if (isLastCharLower && /[a-zA-Z]/.test(c) && c.toUpperCase() === c) {
-			input = input.slice(0, i) + '-' + input.slice(i);
+		if (
+			isLastCharLower &&
+			/[a-zA-Z]/.test(character) &&
+			character.toUpperCase() === character
+		) {
+			string = string.slice(0, i) + "-" + string.slice(i);
 			isLastCharLower = false;
 			isLastLastCharUpper = isLastCharUpper;
 			isLastCharUpper = true;
 			i++;
-		} else if (isLastCharUpper && isLastLastCharUpper && /[a-zA-Z]/.test(c) && c.toLowerCase() === c) {
-			input = input.slice(0, i - 1) + '-' + input.slice(i - 1);
+		} else if (
+			isLastCharUpper &&
+			isLastLastCharUpper &&
+			/[a-zA-Z]/.test(character) &&
+			character.toLowerCase() === character
+		) {
+			string = string.slice(0, i - 1) + "-" + string.slice(i - 1);
 			isLastLastCharUpper = isLastCharUpper;
 			isLastCharUpper = false;
 			isLastCharLower = true;
 		} else {
-			isLastCharLower = c.toLowerCase() === c;
+			isLastCharLower =
+				character.toLowerCase() === character &&
+				character.toUpperCase() !== character;
 			isLastLastCharUpper = isLastCharUpper;
-			isLastCharUpper = c.toUpperCase() === c;
+			isLastCharUpper =
+				character.toUpperCase() === character &&
+				character.toLowerCase() !== character;
 		}
 	}
 
-	return input;
+	return string;
 };
 
-module.exports = (input, options) => {
-	options = Object.assign({
-		pascalCase: false
-	}, options);
+var camelCase = function camelCase(input, options) {
+	if (!(typeof input === "string" || Array.isArray(input))) {
+		throw new TypeError("Expected the input to be `string | string[]`");
+	}
 
-	const postProcess = x => options.pascalCase ? x.charAt(0).toUpperCase() + x.slice(1) : x;
+	options = Object.assign(
+		{
+			pascalCase: false
+		},
+		options
+	);
+
+	var postProcess = function postProcess(x) {
+		return options.pascalCase ? x.charAt(0).toUpperCase() + x.slice(1) : x;
+	};
 
 	if (Array.isArray(input)) {
-		input = input.map(x => x.trim())
-			.filter(x => x.length)
-			.join('-');
+		input = input
+			.map(function(x) {
+				return x.trim();
+			})
+			.filter(function(x) {
+				return x.length;
+			})
+			.join("-");
 	} else {
 		input = input.trim();
 	}
 
 	if (input.length === 0) {
-		return '';
+		return "";
 	}
 
 	if (input.length === 1) {
 		return options.pascalCase ? input.toUpperCase() : input.toLowerCase();
 	}
 
-	if (/^[a-z\d]+$/.test(input)) {
-		return postProcess(input);
-	}
-
-	const hasUpperCase = input !== input.toLowerCase();
+	var hasUpperCase = input !== input.toLowerCase();
 
 	if (hasUpperCase) {
 		input = preserveCamelCase(input);
 	}
 
 	input = input
-		.replace(/^[_.\- ]+/, '')
+		.replace(/^[_.\- ]+/, "")
 		.toLowerCase()
-		.replace(/[_.\- ]+(\w|$)/g, (m, p1) => p1.toUpperCase());
-
+		.replace(/[_.\- ]+(\w|$)/g, function(_, p1) {
+			return p1.toUpperCase();
+		})
+		.replace(/\d+(\w|$)/g, function(m) {
+			return m.toUpperCase();
+		});
 	return postProcess(input);
 };
+
+module.exports = camelCase; // TODO: Remove this for the next major release
+
+module.exports.default = camelCase;
